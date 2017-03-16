d381d4a4bbdddd499b460db34e2ec479d43158ae
Haley Clark <gmail.com@hdeanclark>
2017-03-16 16:42:25 -0700

    Added example to README and migrated to C++11.

--- compile_astarttests.sh
+++ compile_astarttests.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 
-echo "g++ $@  -std=c++0x Utilities.cc astartest_2d_4.cc -o AStarTest_2D_4"
-g++  $@  -std=c++0x Utilities.cc astartest_2d_4.cc -o AStarTest_2D_4
+echo "g++ $@  -std=c++11 Utilities.cc astartest_2d_4.cc -o AStarTest_2D_4"
+g++  $@  -std=c++11 Utilities.cc astartest_2d_4.cc -o AStarTest_2D_4
 
 
-echo "g++ $@  -std=c++0x Utilities.cc astartest_2d_3.cc -o AStarTest_2D_3"
-g++  $@  -std=c++0x Utilities.cc astartest_2d_3.cc -o AStarTest_2D_3
+echo "g++ $@  -std=c++11 Utilities.cc astartest_2d_3.cc -o AStarTest_2D_3"
+g++  $@  -std=c++11 Utilities.cc astartest_2d_3.cc -o AStarTest_2D_3
 
 
-echo "g++ $@  -std=c++0x Utilities.cc astartest_2d_2.cc -o AStarTest_2D_2"
-g++  $@  -std=c++0x Utilities.cc astartest_2d_2.cc -o AStarTest_2D_2
+echo "g++ $@  -std=c++11 Utilities.cc astartest_2d_2.cc -o AStarTest_2D_2"
+g++  $@  -std=c++11 Utilities.cc astartest_2d_2.cc -o AStarTest_2D_2
 
 
-echo "g++ $@  -std=c++0x Utilities.cc astartest_2d.cc -o AStarTest_2D"
-g++  $@  -std=c++0x Utilities.cc astartest_2d.cc -o AStarTest_2D
+echo "g++ $@  -std=c++11 Utilities.cc astartest_2d.cc -o AStarTest_2D"
+g++  $@  -std=c++11 Utilities.cc astartest_2d.cc -o AStarTest_2D
 
 
-echo "g++ $@  -std=c++0x Utilities.cc astartest_3d.cc -o AStarTest_3D"
-g++  $@  -std=c++0x Utilities.cc astartest_3d.cc -o AStarTest_3D
+echo "g++ $@  -std=c++11 Utilities.cc astartest_3d.cc -o AStarTest_3D"
+g++  $@  -std=c++11 Utilities.cc astartest_3d.cc -o AStarTest_3D
